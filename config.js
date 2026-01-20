a32c0946a899cc306929bb897baa2c3baa63c5a1
UPM-UNH <planificacion@unh.edu.pe>
2026-01-20 17:16:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+// config.js
+// URL pública del Google Sheets en formato CSV
+// Hoja: inventario
 
+const SHEET_CSV_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQtYCSC4BZp4eeQcoR8ZtNcuuD80lGAEDt0mrbJJqc6iKefbu5G1zxFmRpk4gByjZy5ZrZBHTddKkFP/pub?output=csv";
